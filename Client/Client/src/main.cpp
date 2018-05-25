184e8db05c5f17e5eb53fc05cdc25ed4a225104c
JustD4Tne <sonny98@interia.pl>
2018-05-25 19:00:17 +0200

    PseudoDocumantation
    Write steps for info about how Server/Client should work
    and delete main.h (i miss clicked last time)

--- Client/Client/src/main.cpp
+++ Client/Client/src/main.cpp
@@ -1,8 +1,31 @@
 #include "Game.h"
-#include "main.h"
 
 int main() {
 	Game* game = new Game();
+
+
+	//	Connect to Server
+
+	//	Get name from Server	
+
+	//	Wait for oponent
+
+	//	Get Packet with informationa about 
+	//	which player start
+
+	//	Loop until Win/Lose/Draw:
+	//		Get sign position (it must be 0 - 8)
+	//		Send to Server and wait for response
+	//		if position was OK then
+	//			Show board and wait for oponent then
+	//				Show board
+	//		else
+	//			go to beginning of loop
+	
+
+	//	Display information about endGame
+	//	Clear board
+	//	
 
 	return 0;
 }
--- Server/Server/src/main.cpp
+++ Server/Server/src/main.cpp
@@ -3,5 +3,32 @@
 int main() {
 	Game* game = new Game();
 
+	//	Wait for PlayerOne
+	//	Create class for him (Send name to him, save IP etc.)
+	
+	//	Wait for PlayerTwo
+	//	Do same things with Player Two
+	 
+	//	Cast lots about which player should start ( O <= always starts)
+	//	Send information about that
+
+	//	Loop until Win/Lose/Draw:
+	//		Wait for Packet about sign position 
+	//		Check if it's OK
+	//		if not then:
+	//			Response about bad position and back to beggining of loop
+	//		else:
+	//			Response about good position
+	//			Check if game is over, if yes then:
+	//				End loop					
+	//			Change player
+	//			Send information about new position from previous player
+	
+	//	Send information to players about GameEnd 
+	//	(Who won/lost or aboout draw)
+
+	//	 Clear board
+	//	 Delete both players
+
 	return 0;
 }
