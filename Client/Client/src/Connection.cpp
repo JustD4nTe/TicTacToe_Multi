903a620df4037e5c4b60a6a87742c9d3cf161a80
JustD4Tne <sonny98@interia.pl>
2018-05-25 22:06:51 +0200

    Client => Game(GetName)
    Connection:
    - Recvall => get sytes from stream/Server
    - GetUInt32_t
    - GetString
    Game:
    - Create constructor
    - Connect => trying every 1s to connect with server, then take a name
    from Server

--- Client/Client/src/Connection.cpp
+++ Client/Client/src/Connection.cpp
@@ -1,3 +1,5 @@
+#pragma comment(lib, "ws2_32.lib")
+
 #include "Connection.h"
 
 Connection::Connection(std::string IP, unsigned int PORT) {
@@ -13,12 +15,51 @@ Connection::Connection(std::string IP, unsigned int PORT) {
 	addr.sin_family = AF_INET; // IPv4 Socket
 }
 
-bool Connection::Connect() {
+bool Connection::ConnectToServer() {
 	Conn = socket(AF_INET, SOCK_STREAM, NULL); // Set Connection socket
 	if (connect(Conn, (SOCKADDR*)&addr, sizeofaddr) != 0) { // If we are unable to connect...
 		MessageBox(NULL, "Failed to Connection", "Error", MB_OK | MB_ICONERROR);
 		return false; // Failed to Connect
 	}
 
+	return true;
+}
+
+//	*data => where store data, totalBytes => how much bytes to recv
+bool Connection::Recvall(char* data, unsigned int totalBytes) {
+	//	When receiving is failed => return false
+	return (recv(Conn, data, totalBytes, NULL) == SOCKET_ERROR ? false : true);
+}
+
+bool Connection::GetUInt32_t(uint32_t& _int32_t){
+	if (!Recvall((char*)&_int32_t, sizeof(uint32_t))) 	//	Try to recv int
+		return false;	//	Return false: int not successfully received
+
+	_int32_t = ntohl(_int32_t);	//	Convert long from Network Byte Order to Host Byte Order
+	return true;	// Return true if we were successful in retrieving the int
+}
+
+bool Connection::GetString(std::string& _string) {
+	unsigned int bufferLength; // Holds length of string
+	//	Get length of string and store it in bufferLength
+	if (!GetUInt32_t(bufferLength))
+		return false;
+
+	//	 Allocate buffer
+	char* buffer = new char[bufferLength + 1];	
+	buffer[bufferLength] = '\0';	//	Add null at end
+
+	// Get string
+	if (!Recvall(buffer, bufferLength)) {
+		// Dellocate buffer memory (cleanup to prevent memory leak)
+		delete[] buffer;
+		return false;
+	}
+
+	_string = buffer;
+
+	// Dellocate buffer memory (cleanup to prevent memory leak)
+	delete[] buffer;
+	
 	return true;
 }
--- Client/Client/src/Game.cpp
+++ Client/Client/src/Game.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <Windows.h>
 
 #include "Game.h"
 
@@ -10,4 +11,20 @@ std::ostream& operator <<(std::ostream& stream, const Board::X_OR_O &Field) {
 Board operator <<(Board& board, const Board::MOVE CurrentPlayer) {
 	board.Array[CurrentPlayer.Position] = CurrentPlayer.Sign;
 	return board;
+}
+
+Game::Game() {
+	Conn = new Connection("127.0.0.1", 111);
+}
+
+void Game::Connect() {
+	while (!Conn->ConnectToServer()){
+		std::cout << "Failed to connect to server..." << std::endl;
+		Sleep(1000);
+	}
+	
+	// Wait 0.1s for response (name)
+	Sleep(100);
+
+	Conn->GetString(PlayerName);
 }
